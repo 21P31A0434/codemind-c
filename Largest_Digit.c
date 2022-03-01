135cdebf9647f5837392390e3a0fc07ce2dac09d
21P31A0434 <98526488+21P31A0434@users.noreply.github.com>
2022-03-01 23:01:07 +0530

    #include<stdio.h> int main() { int num, remider , Largest= 0; scanf("%d", &num); while (num > 0)     { remider = num % 10; if (Largest < remider)     {         Largest = remider;     }     num = num / 10;         }     printf("%d", Largest);     return 0; }

--- Largest_Digit.c
+++ Largest_Digit.c
@@ -1,18 +1,17 @@
 #include<stdio.h>
 int main()
 {
-int num, reminder, Largest=0;
-//printf("Enter the Number:");
+int num, remider , Largest= 0;
 scanf("%d", &num);
-while(num > 0)
+while (num > 0)
     {
-reminder = num % 10;
-if(Largest < reminder)
- {
-     Largest = reminder;
- }
-num = num / 10 ;
+remider = num % 10;
+if (Largest < remider)
+    {
+        Largest = remider;
     }
-printf("%d", Largest);
-return 0;
+    num = num / 10;
+        }
+    printf("%d", Largest);
+    return 0;
 }
